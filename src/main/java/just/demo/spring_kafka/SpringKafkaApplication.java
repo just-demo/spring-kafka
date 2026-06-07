2fb9102bbc987891177003a97c85c0a416b11d74
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-07 22:11:19 +0300

    Example of listener and publisher

--- src/main/java/just/demo/spring_kafka/SpringKafkaApplication.java
+++ src/main/java/just/demo/spring_kafka/SpringKafkaApplication.java
@@ -1,13 +1,37 @@
 package just.demo.spring_kafka;
 
+import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringKafkaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringKafkaApplication.class, args);
+  }
 
+  @KafkaListener(groupId = "demo-group-1", id = "demo-consumer-1", topics = "demo-topic")
+  public void listen1(String message) {
+    System.out.println("demo-consumer-1: " + message);
+  }
+
+  @KafkaListener(groupId = "demo-group-2", id = "demo-consumer-2", topics = "demo-topic")
+  public void listen2(String message) {
+    System.out.println("demo-consumer-2: " + message);
+  }
+
+  @Bean
+  @SuppressWarnings("resource")
+  public ApplicationRunner publish(KafkaTemplate<String, String> template) {
+    return args -> newSingleThreadScheduledExecutor().scheduleAtFixedRate(() ->
+        template.send("demo-topic", "test-" + currentTimeMillis()), 0, 1, SECONDS);
+  }
 }
