4d80bc36a4c1e566e1d0ca2d0b290ba232d627db
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-06-07 20:40:00 +0300

    Starter version

---
+++ src/test/java/just/demo/spring_kafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package just.demo.spring_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
